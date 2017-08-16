514c7f95519c93bad1e006f3d750352337297b45
MacRayy <mecrai@gmail.com>
2017-08-16 17:08:27 +0200

    feat(form): add new dogs with ng-submit

--- angularjs/ninja/app/app.js
+++ angularjs/ninja/app/app.js
@@ -1,27 +1,52 @@
 'use strict'
 
-const app = angular.module('app', []);
+const app = angular.module('app', [])
+.controller('appController', ['$scope', ($scope) => {
 
-app.controller('appController', ['$scope', function($scope) {
+	$scope.removeDog = (dog) => {
+		let removedDog = $scope.dogs.indexOf(dog);
+		$scope.dogs.splice(removedDog, 1);
+	};
+
+	$scope.addDog = () => {
+		$scope.dogs.push({
+			name: $scope.newDog.name,
+			breed: $scope.newDog.breed,
+			hunger: true
+		});
+		$scope.newDog.name = '';
+		$scope.newDog.breed = '';
+	};
+
 	$scope.dogs = [
 		{
 			name: 'Conney',
 			breed: 'border collie',
+			color: 'black',
 			hunger: true
 		},
 		{
 			name: 'Disco',
 			breed: 'collie',
+			color: 'black',
 			hunger: true
 		},
 		{
 			name: 'Lujza',
 			breed: 'mix breed',
+			color: 'black',
 			hunger: false
 		},
 		{
 			name: 'Berta',
 			breed: 'fox terrier',
+			color: 'white',
+			hunger: true
+		},
+		{
+			name: 'Bomper',
+			breed: 'mix breed',
+			color: 'brown',
 			hunger: true
 		}
 	];
