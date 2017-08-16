b8be6a0bfe7aa9e25aa45a0fcd08cc53e2acc31c
MacRayy <mecrai@gmail.com>
2017-08-16 13:48:19 +0200

    feat(header): header added

--- angularjs/ninja/app/app.js
+++ angularjs/ninja/app/app.js
@@ -1,7 +1,20 @@
 const app = angular.module('app', []);
 
-app.controller('appController', function($scope) {
+app.controller('appController', ['$scope', function($scope) {
 	$scope.message = 'hey';
 
-	$scope.dogs = ['berta', 'conney', 'disco', 'lujza'];
-});
+	$scope.dogs = [
+		{
+			name: 'Conney',
+			breed: 'border collie'
+		},
+		{
+			name: 'Lujza',
+			breed: 'mix breed'
+		},
+		{
+			name: 'Berta',
+			breed: 'fox terrier'
+		}
+	];
+}]);
