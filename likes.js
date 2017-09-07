ca4a018c8352dcb9fde2c3fc463aa59e034960cd
MacRayy <mecrai@gmail.com>
2017-09-07 15:25:22 +0200

    feat(likes): works with more names

--- likes.js
+++ likes.js
@@ -1,9 +1,17 @@
 const likes = (names) => {
 	let likeMsg = 'no one likes this'
 	if (typeof names[0] === 'string') {
-		likeMsg = names[0] + ' likes this'
+		if (names.length === 1) {
+			likeMsg = names[0] + ' likes this'
+		} else if (names.length === 2) {
+			likeMsg = names[0] + ' and ' + names[1] + ' like this'
+		} else if (names.length === 3) {
+			likeMsg = names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'
+		} else {
+			likeMsg = names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this'
+		}
 	}
 	return likeMsg
 }
 
-console.log(likes(['peter']))
+console.log(likes(["Max", "John", "Mark"]))
