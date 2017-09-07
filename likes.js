e346d0d97cf95f71cc6392149f50d54a91df3243
MacRayy <mecrai@gmail.com>
2017-09-07 14:59:55 +0200

    feat(likes): works with empty array

--- likes.js
+++ likes.js
@@ -1,3 +1,11 @@
-function likes(names) {
-  // TODO
+const likes = (names) => {
+	let likeMsg = ''
+	if (names = []) {
+		likeMsg = 'no one likes this'
+	} else {
+
+	}
+	return likeMsg
 }
+
+console.log(likes(["Peter"]))
