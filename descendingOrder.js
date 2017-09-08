ed683c258a2010d97eba3d9754ba5ed6cb70e71f
MacRayy <mecrai@gmail.com>
2017-09-08 14:19:12 +0200

    feat(refactor): function shorten

--- descendingOrder.js
+++ descendingOrder.js
@@ -1,9 +1,7 @@
 const descendingOrder = (n) => {
-	let newNum = 0
 	let array = n.toString().split('')
 	array = array.map((num) => parseInt(num))
-	newNum = parseInt(array.sort().reverse().join(''))
-	return newNum
+	return parseInt(array.sort().reverse().join(''))
 }
 
 console.log(descendingOrder(12539));
