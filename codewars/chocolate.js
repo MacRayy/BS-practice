6da918bdda5ef559aa3b1210c57cf09f6cfa743f
MacRayy <mecrai@gmail.com>
2017-09-08 13:57:55 +0200

    feat(choc): working 🍫

---
+++ codewars/chocolate.js
@@ -0,0 +1,9 @@
+const breakChocolate = function(n, m) {
+ if (typeof n !== 'number' || typeof m !== 'number') {
+   return 0
+  } else {
+   return (n * m) - 1
+  }
+};
+
+console.log(breakChocolate(1, -1));
